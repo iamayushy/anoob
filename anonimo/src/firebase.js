9c5421894489d70c7c79b84fa250d55a42ba6c06
iamayushy <ayushhuns@gmai.com>
2022-06-02 20:05:57 +0530

    project-init

---
+++ anonimo/src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAO4Azi37Pv3p6BEI1-pT5EnMv4Zmpte-s",
+  authDomain: "disgus-baf6f.firebaseapp.com",
+  projectId: "disgus-baf6f",
+  storageBucket: "disgus-baf6f.appspot.com",
+  messagingSenderId: "876506827176",
+  appId: "1:876506827176:web:2c3f61cb553e64e3c5359d"
+};
+
+const app = initializeApp(firebaseConfig);
